3a25883c0a83d8f6d0c8543b844795f39b4342e0
schoolman88 <125292272+schoolman88@users.noreply.github.com>
2023-11-30 13:18:51 +0800

    Delete MineSweeperRenewed 3 directory

--- MineSweeperRenewed 3/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.minesweeperrenewed {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.minesweeperrenewed to javafx.fxml;
-    exports com.example.minesweeperrenewed;
-}
